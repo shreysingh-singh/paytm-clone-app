2940a2821d9e7a59ea5ff79da6c18b94080b51fa
Shrey <shreysingh11733@gmail.com>
2026-05-06 20:50:50 +0530

    Fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import { Signup } from "./pages/Signup";
 import Signin from "./pages/Signin";
@@ -10,6 +10,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="*" element={<Navigate to="/signin" />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
